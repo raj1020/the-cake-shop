f34e491f731bc1e432003513b924aaa8111918f7
raj1020 <raj.1020@hotmail.com>
2020-02-14 23:48:55 +1100

    added store to the application

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+const redux = require('redux');
+const createStore = redux.createStore;
+
+
 const BUY_CAKE = 'BUY_CAKE';
 
 function buyCake() {
@@ -21,3 +26,11 @@ const reducer = (state = initialState, action) => {
         default: return state;
     }
 }
+
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
