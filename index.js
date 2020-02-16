740f34a2eb7be573f583b3349127be879403f1b0
raj1020 <raj.1020@hotmail.com>
2020-02-16 13:24:12 +1100

    added a middleware name 'logger' which  provides a log of actions associated with executing the application

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
+
 const redux = require('redux');
+const reduxLogger = require('redux-logger');
 const createStore = redux.createStore;
 const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
 
 
 const BUY_CAKE = 'BUY_CAKE';
@@ -60,9 +64,9 @@ const cakeReducer = (state = initialCakeState, action) => {
     iceCream: iceCreamReducer
  }); 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('Initial state', store.getState());
-const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
