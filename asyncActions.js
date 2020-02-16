e42e4576484ab2215c0c02f8a24a31b172160718
raj1020 <raj.1020@hotmail.com>
2020-02-16 16:07:40 +1100

    added async actions for fetching the list of users

---
+++ asyncActions.js
@@ -0,0 +1,32 @@
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+};
+
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUEST
+    }
+
+}
+
+const fetchUsersSuccess = users => {
+    return {
+        type: FETCH_USERS_SUCCESS,
+        payload: users 
+    }
+}
+
+const fetchUsersFailure = error => {
+    return {
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+    
+}
