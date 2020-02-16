d555a4f5a8db94e4cf199f58c0bdbcdea0f68b36
raj1020 <raj.1020@hotmail.com>
2020-02-16 11:58:31 +1100

    added functionality for buying icecream by modifying reducer and adding actions for buying Icecream

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const createStore = redux.createStore;
 
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 function buyCake() {
     return {
@@ -13,8 +14,16 @@ function buyCake() {
 }
 
 
+function buyIceCream () {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
+
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIceCreams: 20
 };
 
 const reducer = (state = initialState, action) => {
@@ -22,10 +31,16 @@ const reducer = (state = initialState, action) => {
         case BUY_CAKE: return {
             ...state,
             numOfCakes: state.numOfCakes-1
-        };
+        }
+        
+            case BUY_ICECREAM: return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams-1
+            }
         default: return state;
     }
-}
+  }
+
 
 const store = createStore(reducer);
 console.log('Initial state', store.getState());
@@ -33,4 +48,6 @@ const unsubscribe = store.subscribe(() => console.log('Updated state', store.get
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
